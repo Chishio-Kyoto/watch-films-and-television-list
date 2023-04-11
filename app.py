84dcda4a777508ea288654acbfd53fd81a056936
Chishio-Kyoto <houle.cf@outlook.com>
2023-04-11 10:58:35 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 from markupsafe import escape
 
 
 # __name__ 为'__main__'
 app = Flask(__name__)
 
+# 定义虚拟数据
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
 
 @app.route('/')
 @app.route('/index')
+def index():
+    # return '欢迎来到我的 Watchlist!'
+    return render_template('index.html', name=name, movies=movies)
+
+
+@app.route('/hello')
 def hello():
     return '欢迎来到我的 Watchlist!'
 
